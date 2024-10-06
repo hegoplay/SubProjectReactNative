24a4fa11c04fb6f84565715e20f4b70531cb9f4a
H82M19\Student <Student>
2024-10-06 13:41:26 +0700

    update constant color

---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+const Colors = {
+    primary100: "#eee0fa",
+    primary300: "#c393f9",
+    primary500: '#7c25ee'
+}
---
+++ screens/SignIn.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+const SignIn = () => {
+    return <View>
+        <Text> Test</Text>
+    </View>
+}
+
+export { SignIn } 
